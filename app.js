0a786c67ef3ed522059cc7e4743d6f0502ce12b6
Sefa Keles <sefaaakeles@gmailcom>
2021-12-18 05:54:17 +0000

    Controllers modified

--- app.js
+++ app.js
@@ -23,19 +23,20 @@ const ProductController = (function(){
     //DUMMY DATA
     const data = {
         products: [
-            {
-                id: 1, name: "Dell Monitor", price: 170
-            },
-            {
-                id: 2, name: "Philips Monitor", price: 110
-            },
-            {
-                id: 3, name: "MSI Monitor", price: 100
-            }
+            // {
+            //     id: 1, name: "Dell Monitor", price: 170
+            // },
+            // {
+            //     id: 2, name: "Philips Monitor", price: 110
+            // },
+            // {
+            //     id: 3, name: "MSI Monitor", price: 100
+            // }
         ],
         selectedProduct: null,
         totalPrice: 0
     }
+    
 
     //Public Section
     return {
@@ -44,6 +45,20 @@ const ProductController = (function(){
         },
         getData: function(){
             return data;
+        },
+        addProduct: function(name, price){
+
+            //Editing The Product Id
+            let id;
+            if(data.products.length > 0){
+                id = data.products[data.products.length-1].id+1;                
+            }else{
+                id = 0;
+            }
+
+            const newProduct = new Product(id, name, price);
+            data.products.push(newProduct);
+            return newProduct;
         }
     }
         
@@ -57,7 +72,11 @@ const UiController = (function(){
 
     //Selectors Types For Querry Selector
     const selectors = {
-        productList: "#item-list"
+        productList: "#item-list",
+        productName: "#productName",
+        productPrice: "#productPrice",
+        addBtn: ".addBtn",
+        productCard: "#productCard"
     }
 
     return {
@@ -84,6 +103,32 @@ const UiController = (function(){
         },
         getSelectors: function(){
             return selectors;
+        },
+        addProductToList: function(prd){
+
+            document.querySelector(selectors.productCard).style.display = "block";
+
+            let item = `
+                <tr>
+                    <td>${prd.id}</td>
+                    <td>${prd.name}</td>
+                    <td>${prd.price}</td>
+                    <td class="text-right">
+                        <button type="submit" class="btn btn-warning btn-sm">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                    </td>
+                </tr>
+            `;
+
+            document.querySelector(selectors.productList).innerHTML += item;
+        },
+        clearInputs: function(){
+            document.querySelector(selectors.productName).value = "";
+            document.querySelector(selectors.productPrice).value = "";
+        },
+        hideCardList: function(){
+            document.querySelector(selectors.productCard).style.display = "none";
         }
     }
 
@@ -93,15 +138,52 @@ const UiController = (function(){
 
 // APP CONTROLLER (IMMEDIATE FUNCTION)
 const AppController = (function(productController, uiController){
- 
+    
+    const selectors = uiController.getSelectors();
+
+    // Where  All Event Listeners Are Managed
+    const loadEventListeners = function(){
+        
+        //Add Product
+        document.querySelector(selectors.addBtn).addEventListener("click", submitAddProduct);
+    }
+
+    const submitAddProduct = (e) =>{
+
+        const productName = document.querySelector(selectors.productName).value;
+        const productPrice = document.querySelector(selectors.productPrice).value;
+
+        if(productName !=="" || productPrice !==""){
+
+            //Add Product
+            const newProduct = productController.addProduct(productName, parseFloat(productPrice));
+            
+            //Add Product To UI List
+            uiController.addProductToList(newProduct);
+
+            // Clear Inputs
+            uiController.clearInputs();
+        }
+
+        e.preventDefault();
+    }
 
     return {
         //Bu fonksiyon, hangi modulun ilk once baslatilacagini yani tetiklenecegini belirler. Bu yuzden onemlidir. 
         init: function(){
             console.log("App is starting...")
             const products = productController.getProducts();
+
+            if(products.length === 0){
+                uiController.hideCardList();
+            }else{
+                uiController.createProductList(products);
+            }
            
-            uiController.createProductList(products);
+            
+
+            //Load Event Listener
+            loadEventListeners();
         }
     }
 
